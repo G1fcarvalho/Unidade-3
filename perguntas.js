838057f7327689597d35767079bca2792b303a2f
G1fcarvalho <0000108507416xsp@al.educacao.sp.gov.br>
2024-11-21 13:26:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'primeira pergunta sobre a Gigi',
+    'Qual o meu nome completo? e qual a data que eu nasci?',
+    'Giovanna Carvalho Flor Da Silva, nasceu em 2007 no dia 03/07'
+)
+
+criaCartao(
+    'Segunda pergunta sobre a Gigi',
+    'Qual foi o único outro animal que ela teve, além da Crystal?',
+    'A Beth, a jabuti'
+)
+
+criaCartao(
+    'Terceira pergunta sobre a Gigi',
+    'Qual o meu projeto de vida? (em que eu quero me formar, em que área quero trabalhar e quantos filhos eu quero ter?)',
+    'Pretende se formar em Biomed, abrir uma clínica de estética e quer ter um casal de gêmeos (Helena e Heitor)'
+)
+
+criaCartao(
+    'Quarta pergunta sobre a Gigi',
+    'Qual foi o castigo mais traumático da Gigi? ',
+    'Ter ficado ajoelhada no milho por mentir pra mãe, hoje ri horrores contando a história mas é algo que nunca mais vai esquecer.'
+)
